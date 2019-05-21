676bbf1b1bedffef7117a3b95282851614da5258
Nia Bert <nbert14@gmail.com>
2019-05-21 23:39:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
 
+if array.length == 2
+  return array.join(" and ")
+
+ elsif array.length == 1
+   return array.join
+   
+elsif array.length > 2
+array[0..-2].join(", ") + ", and " + array[-1]
+
 end
+end
+
