49cf447f79e157fe1922d2b6081a5f2d408f0433
Nia Bert <nbert14@gmail.com>
2019-05-21 23:38:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,14 +2,15 @@ def oxford_comma(array)
 
 if array.length == 2
   return array.join(" and ")
-  
-elsif array.length > 2
-array[0..-2].join(', ') + ", and " + array[-1]
- 
- return array.join(" ")
- 
+
  elsif array.length == 1
    return array.join
+   
+elsif array.length > 2
+array[0..-2].join(", ") + ", and " + array[-1]
+ 
+ return array.join(" ")
+
 end
 end
 
